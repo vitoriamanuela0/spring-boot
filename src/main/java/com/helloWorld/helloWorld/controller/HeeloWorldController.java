ec87dab66307ab9e8b3679f5cf62b21cd24f61f8
vitoriamanuela0 <contatodavitoria@gmail.com>
2024-11-26 16:00:05 -0300

    SpringBoot

---
+++ src/main/java/com/helloWorld/helloWorld/controller/HeeloWorldController.java
@@ -0,0 +1,33 @@
+package com.helloWorld.helloWorld.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+
+public class HeeloWorldController {
+
+@GetMapping
+	public String helloworld() {
+		return "Hello World!!!";
+	}
+	
+@GetMapping("/bsm")
+    public List<String> bsmList() {
+        return Arrays.asList(
+            "Orientação ao futuro.",
+            "Responsabilidade pessoal.",
+            "Mentalidade de crescimento.",
+            "Persistência.",
+            "Comunicação.",
+            "Trabalho em equipe.",
+            "Atenção aos detalhes.",
+            "Proatividade."
+        );
+    }
+}
